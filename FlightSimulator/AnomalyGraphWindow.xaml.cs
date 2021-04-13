d319ee5e7d71dbd6feb9056fe8d1b4e8503e0f64
Noam Koren <noamko25@gmail.com>
2021-04-13 06:00:26 -0700

    Added Dynamic dll Loader

--- FlightSimulator/AnomalyGraphWindow.xaml.cs
+++ FlightSimulator/AnomalyGraphWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Windows;
@@ -18,24 +21,53 @@ namespace FlightSimulator
     /// </summary>
     public partial class AnomalyGraphWindow : Window
     {
+        string normal_flight_csv;
+        string anomaly_flight_csv;
         public AnomalyGraphWindow()
         {
             InitializeComponent();
             anomaly_uc.Visibility = Visibility.Hidden;
         }
-
+        bool loadDLL()
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.DefaultExt = ".dll";
+            fd.Filter = "DLL Files (*.dll)|*.dll";
+            Nullable<bool> result = fd.ShowDialog();
+            if (result == true)
+            {
+                File.Copy(fd.FileName, Environment.CurrentDirectory + @"/AnomalyDetectionLib.dll",true);
+                return true;
+            }
+            return false;
+        }
         public void LoadCSV(string normal, string anomaly)
         {
-            new Thread(() =>
+            normal_flight_csv = normal;
+            anomaly_flight_csv = anomaly;
+            if (loadDLL())
             {
-                Thread.CurrentThread.IsBackground = true;
-                anomaly_uc.LoadCSVS(normal, anomaly);
-                Dispatcher.BeginInvoke(new Action(() =>
+                new Thread(() =>
                 {
-                    anomaly_uc.Visibility = Visibility.Visible;
-                    txt_loading.Visibility = Visibility.Hidden;
-                }));
-            }).Start();
+                    Thread.CurrentThread.IsBackground = true;
+                    Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        anomaly_uc.Visibility = Visibility.Hidden;
+                        txt_loading.Visibility = Visibility.Visible;
+                    }));
+                    anomaly_uc.LoadCSVS(normal, anomaly);
+                    Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        anomaly_uc.Visibility = Visibility.Visible;
+                        txt_loading.Visibility = Visibility.Hidden;
+                    }));
+                }).Start();
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            LoadCSV(normal_flight_csv,anomaly_flight_csv);
         }
     }
 }
